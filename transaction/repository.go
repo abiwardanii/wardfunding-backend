515553ad392131ce11d79ea3f828ce1d11c53c98
abi wardani <abiwardani@abis-MacBook-Pro.local>
2021-06-10 17:25:47 +0700

    campaign transaction handler

--- transaction/repository.go
+++ transaction/repository.go
@@ -3,7 +3,7 @@ package transaction
 import "gorm.io/gorm"
 
 type repository struct {
-	db *gorm.DB
+		db *gorm.DB
 }
 
 type Repository interface {
@@ -15,11 +15,11 @@ func NewRepository(db *gorm.DB) *repository {
 }
 
 func (r *repository) GetByCampaignID(campaignID int) ([]Transaction, error) {
-	var transaction []Transaction
+	var transactions []Transaction
 
-	err := r.db.Preload("User").Where("campaign_id = ?", campaignID).Find(&transaction).Error
+	err := r.db.Preload("User").Where("campaign_id = ?", campaignID).Find(&transactions).Error
 	if err != nil {
-		return transaction, err
+		return transactions, err
 	}
-	return transaction, nil
+	return transactions, nil
 }
