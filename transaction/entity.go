94b913439b1d958286e51bcba46d15af88a457dd
abi wardani <abiwardani@abis-MacBook-Pro.local>
2021-06-11 19:25:09 +0700

    User transaction repository

--- transaction/entity.go
+++ transaction/entity.go
@@ -2,6 +2,7 @@ package transaction
 
 import (
 	"time"
+	"wardfunding/campaign"
 	"wardfunding/user"
 )
 
@@ -13,6 +14,7 @@ type Transaction struct {
 	Status     string
 	Code       string
 	User	   user.User
+	Campaign   campaign.Campaign
 	CreatedAt  time.Time 
 	UpdatedAt  time.Time
 }
