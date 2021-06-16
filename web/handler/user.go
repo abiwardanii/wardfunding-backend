51897af1de30f57148a5a4efe72ec1d711584c1e
abi wardani <abiwardani@abis-MacBook-Pro.local>
2021-06-16 16:09:35 +0700

    create user submit

--- web/handler/user.go
+++ web/handler/user.go
@@ -28,3 +28,25 @@ func(h *userHandler) Index(c *gin.Context) {
 func (h *userHandler) New(c *gin.Context) {
 	c.HTML(http.StatusOK, "user_new.html", nil)
 }
+
+func (h *userHandler) Create(c *gin.Context) {
+	var input user.FormCreateUserInput
+
+	err := c.ShouldBind(&input)
+	if err != nil {
+
+	}
+
+	registerInput := user.RegisterUserInput{}
+	registerInput.Name = input.Name
+	registerInput.Email = input.Email
+	registerInput.Occupation = input.Occupation
+	registerInput.Password = input.Password
+
+	_, err = h.userService.RegisterUser(registerInput)
+	if err != nil {
+
+	}
+
+	c.Redirect(http.StatusFound, "users")
+}
