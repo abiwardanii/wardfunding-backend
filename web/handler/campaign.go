374b151b371f1ea924df90c519a77795f1c0587f
abi wardani <abiwardani@abis-MacBook-Pro.local>
2021-06-18 22:44:05 +0700

    create campaign form

--- web/handler/campaign.go
+++ web/handler/campaign.go
@@ -3,16 +3,18 @@ package handler
 import (
 	"net/http"
 	"wardfunding/campaign"
+	"wardfunding/user"
 
 	"github.com/gin-gonic/gin"
 )
 
 type campaignHandler struct {
 	campaignService campaign.Service
+	userService		user.Service
 }
 
-func NewCampaignHandler(campaignService campaign.Service) *campaignHandler {
-	return &campaignHandler{campaignService}
+func NewCampaignHandler(campaignService campaign.Service, userService user.Service) *campaignHandler {
+	return &campaignHandler{campaignService, userService}
 }
 
 func (h *campaignHandler) Index(c *gin.Context) {
@@ -22,4 +24,17 @@ func (h *campaignHandler) Index(c *gin.Context) {
 		return 
 	}
 	c.HTML(http.StatusOK, "campaign_index.html", gin.H{"campaigns": campaigns})
+}
+
+func (h *campaignHandler) New(c *gin.Context) {
+	users, err := h.userService.GetAllUsers()
+	if err != nil {
+		c.HTML(http.StatusInternalServerError, "error.html", nil)
+		return
+	}
+
+	input := campaign.FormCreateCampaignInput{}
+	input.Users = users
+
+	c.HTML(http.StatusOK, "campaign_new.html", input)
 }
